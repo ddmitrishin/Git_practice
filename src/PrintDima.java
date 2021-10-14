e6713842700f77df96a1071a8f06f4977603eff9
ddmitrishin <ddmitrishin@appliedtech.ru>
2021-10-14 12:25:24 +0500

    add start

--- src/PrintDima.java
+++ src/PrintDima.java
@@ -3,5 +3,6 @@ public class PrintDima {
     public static void main(String[] args) {
         String userName = "Dima";
         System.out.println("My name is " + userName);
+        System.out.println("start");
     }
 }
